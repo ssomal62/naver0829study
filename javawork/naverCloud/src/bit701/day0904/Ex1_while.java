89a71c3ea5343d0b81b71bad16371393f9deddd3
ssomal <wxy890@gmail.com>
2023-09-07 09:36:16 +0900

    최초 이클립스 git연동

--- javawork/naverCloud/src/bit701/day0904/Ex1_while.java
+++ javawork/naverCloud/src/bit701/day0904/Ex1_while.java
@@ -3,7 +3,6 @@ package bit701.day0904;
 public class Ex1_while {
 	public static void main(String[] args) {
 
-		
 		int a = 'A';
 		while (a <= 'Z') {
 			System.out.print((char) a++ + " ");// 오오 이렇게도 되는구나..
@@ -11,7 +10,6 @@ public class Ex1_while {
 		}
 		System.out.println();
 
-		
 		a = 'A';
 		while (true) {
 			System.out.print((char) a++ + " ");
@@ -21,34 +19,29 @@ public class Ex1_while {
 		}
 		System.out.println();
 
-		
 		a = 'A';
 		do {
 			System.out.print((char) a++ + " ");
-		} while (a <= 90);// 조건 뒤에 반드시 ; 
+		} while (a <= 90);// 조건 뒤에 반드시 ;
 		System.out.println();
-		
+
 //		int b = 4;
 //		Integer c = 4;
 //		System.out.println(b+c);
-		
-		
+
 		int n = 10;
-		
-		while(n<10) {
+
+		while (n < 10) {
 			System.out.println(n--);
 		}
-		
+
 		do {
 			System.out.print(n-- + " ");
-			if(n==0) {
+			if (n == 0) {
 				break;
 			}
-		}while(n<10);
-		
-		
-		
-		
+		} while (n < 10);
+
 		/*
 		 * Ex1_while test1 = new Ex1_while(); Ex1_while test2 = new Ex1_while();
 		 * 
@@ -56,18 +49,12 @@ public class Ex1_while {
 		 * 
 		 * boolean check = test1.equals(test2); System.out.println(check);
 		 */
-		
-		
-		
-		
+
 	}
-	
-	
-public void Test() {
-	System.out.println("객체 테스트용입니다.");
-}
 
 
+	public void Test() {
+		System.out.println("객체 테스트용입니다.");
+	}
+
 }
-	
-	
